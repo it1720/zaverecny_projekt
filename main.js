6a7825b99d46ffee5633991680535c35bf8d964f
it1720 <36028694+it1720@users.noreply.github.com>
2020-12-08 18:00:52 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Problém s CORS
- $(document).ready(function() {
-    $.getJSON("data.json",function(data){
-        $.each(data,function(key,value){
-            console.log(value.name)
-        });
-    });
-});
